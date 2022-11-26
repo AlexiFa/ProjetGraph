f71d5a4640706f0e696720f9352486ef0e5a4c6d
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2022-11-26 20:08:31 +0100

    création de la matrice fini + début du test de circuit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ for ligne in lines:
     i = 0
     k = 0
     var = ""
-
+del var,i,k,tab_inter,ligne,j ## suppr les variable temp qu'on a créer pour fabriquer le tableau
 # for val in tab_ligne:
 #     print(val)
 
@@ -56,11 +56,52 @@ for ligne in tab_ligne:
 # ajout de alpha et oméga
 tab_ligne = [[0,0]]+tab_ligne+[tab_omega] # vérifier si les prédécesseurs de oméga sont bien ceux qui n'apparaissent pas dans les prédecesseurs
 
-for val in tab_ligne:
-    print(val)
+del tab_pre,found,tab_omega,i,val,j,ligne
+
+# for val in tab_ligne:
+#     print(val)
 
 ### création du graph ### (sous forme de matrice de valeurs) # on va dire matrice d'adjacence pour l'instant (demander si c bien cette matrice)
 matrice = []
+match = 0
+temp = []
 for valH in tab_ligne:
     for valV in tab_ligne:
-        TODO='faire la matrice'
+        if (len(valV)>=3):
+            for i in range(2,len(valV)): # vérifier
+                if (valH[0]==valV[i]):
+                    match = 1
+        temp.append(match)
+        match = 0
+    matrice.append(temp)
+    temp = []
+
+# for val in matrice:
+#     print(val)
+
+del match,temp,valV,valH,i
+
+## on a ajouter le point d'entrée et le point de sortie manuellement donc on est sur qu'il y en a qu'un de chaque
+
+## vérification qu'il n'y ai pas de circuit ##
+
+# stocker les predececeurs et les successeurs
+for val in tab_ligne:
+    print(val)
+tab_pre = []
+for sommet in tab_ligne:
+    if(len(sommet)>=3):
+        for pre in sommet:
+            tab_pre.append(pre)
+
+for val in tab_pre:
+    print(val)
+
+## methode de Roy-Warshall
+
+i = 0
+MA_trans = matrice # matrice d'adjacence de la fermueture transitive
+for sommet in range(0,15):
+    for prede in tab_ligne[sommet]:
+        for succ in matrice[sommet]:
+            a=2
